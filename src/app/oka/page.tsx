2d71b3cd64e8410e744cdd281798956a5a389f17
PrimeJokyu <primejokyu@gmail.com>
2025-04-05 15:06:52 +0900

    oka20250405

---
+++ src/app/oka/page.tsx
@@ -0,0 +1,4 @@
+export default function Home() {
+  return <div className="h-full"><div className="rounded-full bg-red-400 w-10 h-10">　</div><div>
+    <p className="text-7xl text-blue-500">×</p></div></div>;
+}
