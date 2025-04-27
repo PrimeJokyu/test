d78acaeee2473b2f707de9786e1d7ad0bc187d5d
PrimeJokyu <primejokyu@gmail.com>
2025-04-27 10:35:25 +0900

    赤い丸を９個作った

--- src/app/oka/page.tsx
+++ src/app/oka/page.tsx
@@ -1,21 +1,17 @@
 export default function Home() {
   return (
-    <>
-      <div className="flex space-x-4 space-y-4">
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-      </div>
-      <div className="flex space-x-4 space-y-4">
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-      </div>
-      <div className="flex space-x-4 space-y-4">
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-        <div className="w-[100px] h-[100px] bg-red-500 rounded-full"></div>
-      </div>
+    //TODO: 丸同士の間隔を均等に
+    <><div className="grid grid-cols-3 gap-4 p-4">
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+  </div>
     </>
   );
 }
