c17faf2a8797e1432b6041f75f75d68e71813190
PrimeJokyu <primejokyu@gmail.com>
2025-05-11 13:45:40 +0900

    エラー解消のためいったんプッシュ

--- src/app/yamamoto/api/page.tsx
+++ src/app/yamamoto/api/page.tsx
@@ -1,24 +1,28 @@
 "use client";
 import { useState } from "react";
 
-interface  A {
-    name:string,
-    type:string
-};
-
-const pokemon= () => {
-    const [poke,setPoke] = useState<A>()
-    const a ={
-        name : "ピカチュウ",
-        type : "電気"
-};
-const b = () => {
-    setPoke(a)
+interface A {
+  name: string;
+  type: string;
 }
-return<div>
-<div  style={{backgroundColor: "red", padding:"1px", margin :"1px" }}    >{a.name}{poke?.name}</div>
-<button  onClick={b}>o</button>
-</div>
 
+const Pokemon = () => {
+  const [poke, setPoke] = useState<A>();
+  const a = {
+    name: "ピカチュウ",
+    type: "電気",
+  };
+  const b = () => {
+    setPoke(a);
+  };
+  return (
+    <div>
+      <div style={{ backgroundColor: "red", padding: "1px", margin: "1px" }}>
+        {a.name}
+        {poke?.name}
+      </div>
+      <button onClick={b}>o</button>
+    </div>
+  );
 };
-export default pokemon;
+export default Pokemon;
